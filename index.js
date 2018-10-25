fa2869dfb6907d62ee245f52d924fb9026659bbd
mvoss9000 <mvoss9000@yahoo.com>
2018-10-25 01:26:35 -0400

    slim down functions with shared logic

--- index.js
+++ index.js
@@ -19,64 +19,13 @@
 
 var hmap = require('qb-hmap')
 
-function err(msg) {
-    throw Error(msg)
-}
 
-var PS_KEY_FNS = {
-    hash_fn: function (args) {
-        var ps = args[0]
-        var src = ps.src
-        var lim = ps.klim - 1                       // ignore quotes
-        var h = 0
-        for (var i = ps.koff + 1; i < lim; i++) {
-            h = 0x7FFFFFFF & ((h * 33) ^ src[i])    // xor by berstein
-        }
-        return h
-    },
-    equal_fn: function (key, args) {
-        var ps = args[0]
-        var len = ps.klim - ps.koff - 2             // ignore quotes
+function Buf (src, off, lim, hash, col) {
+    var len = lim - off
+    var nsrc = new Uint8Array(len)
+    for (var i = 0; i < len; i++) { nsrc[i] = src[off + i] }
 
-        len >= 0 || err('no key')
-
-        var ksrc = key.src
-        if (ksrc.length !== len) {
-            return false
-        }
-
-        var psrc = ps.src
-        var poff = ps.koff + 1
-        for (var i = 0; i < len; i++) {
-            if (ksrc[i] !== psrc[i + poff]) {
-                return false
-            }
-        }
-        return true
-    },
-    create_fn: function (hash, col, prev, args) {
-        if (prev) {
-            return prev
-        }
-        var ps = args[0]
-        var poff = ps.koff + 1              // ignore quotes
-        var psrc = ps.src
-        var len = ps.klim - ps.koff - 2     // ignore quotes
-        len >= 0 || err('no key')
-        var src = new Uint8Array(len)
-        for (var i = 0; i < len; i++) {
-            src[i] = psrc[i + poff]
-        }
-        return new Buf(src, hash, col)
-    },
-    str2args_fn: function (s) {
-        var b = new Buffer('"' + s + '"')
-        return [{src: b, koff: 0, klim: b.length}]
-    },
-}
-
-function Buf(src, hash, col) {
-    this.src = src
+    this.src = nsrc
     this.hash = hash
     this.col = col
     this.str = null
@@ -95,6 +44,54 @@ Buf.prototype = {
     },
 }
 
+var PS_VAL_FNS = {
+
+}
+
+function hash (src, off, lim) {
+    for (var i = off, h = 0; i < lim; i++) {
+        h = 0x7FFFFFFF & ((h * 33) ^ src[i])    // xor by berstein
+    }
+    return h
+}
+
+function src_equal (src1, src2, off, lim) {
+    var len = src1.length
+    if (lim - off !== len) {
+        return false
+    }
+    for (var i=0; i<len; i++) {
+        if (src1[i] !== src2[off + i]) {
+            return false
+        }
+    }
+    return true
+}
+
+var PS_KEY_FNS = {
+    hash_fn: function (args) {
+        var ps = args[0]
+        return hash(ps.src, ps.koff + 1, ps.klim - 1)
+    },
+    equal_fn: function (key, args) {
+        var ps = args[0]
+        return src_equal(key.src, ps.src, ps.koff + 1, ps.klim -1)
+    },
+    create_fn: function (hash, col, prev, args) {
+        if (prev) {
+            return prev
+        }
+        var ps = args[0]
+        return new Buf(ps.src, ps.koff + 1, ps.klim -1, hash, col)
+    },
+    str2args_fn: function (s) {
+        var b = new Buffer('"' + s + '"')
+        return [{ src: b, koff: 0, klim: b.length }]
+    },
+}
+
 module.exports = {
-    create: function () { return hmap.set(PS_KEY_FNS) },
+    create: function () {
+        return hmap.set(PS_KEY_FNS)
+    },
 }
